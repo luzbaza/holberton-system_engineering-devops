de489e63b94710ce8335c67f57e3571dc859331c
luzbaza <2459@holbertonschool.com>
2021-07-13 15:44:44 -0500

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- MYSQL
+--- lists all privileges of the MySQL!
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
