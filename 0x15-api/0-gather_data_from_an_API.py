b10f052f0fce064127be9714d1f8c6bd4bbbf943
luzbaza <2459@holbertonschool.com>
2021-09-13 20:38:59 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,16 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    tasks = 0
-    completed = 0
+
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
     completed_tasks = []
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{:}'
-                     .format(argv[1])).json()
-
-    r2 = requests.get('https://jsonplaceholder.typicode.com/todos/?userId={:}'
-                      .format(argv[1])).json()
-
-    for task in r2:
-        tasks += 1
+    for task in todo:
         if task.get('completed') is True:
-            completed += 1
             completed_tasks.append(task.get('title'))
-    print("Employee {:} is done with tasks({:}/{:}):".format(
-        r.get('name'), completed, tasks))
-    for item in completed_tasks:
-        print("\t {:}".format(item))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
