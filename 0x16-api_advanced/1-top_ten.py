b39f19ed8a8cbea373c4b8c6f23497dfd35d1d6b
luzbaza <2459@holbertonschool.com>
2021-09-27 21:46:44 -0500

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,16 @@ import requests
 
 
 def top_ten(subreddit):
-    sub_info = requests.get("https://www.reddit.com/r/{}/host.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-
-    if sub_info.status_code >= 300:
-        print('None')
+    header = {"User-Agent": "Holberton"}
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code == 200:
+        for i in r.json().get("data", None).get("children", None):
+            print(i.get("data", None).get("title", None))
 
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        print(None)
+
+
+if __name__ == "__main__":
+    pass
